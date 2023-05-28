6ff9bdea6675b70b328419a4930b58f8530b7380
anish <anishtharur@gmail.com>
2023-05-28 22:03:09 +0530

    auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,6 +46,7 @@ export const login = async (req, res) => {
     if (!user) res.status(400).json({ message: "User doesnt exist" });
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) res.status(400).json({ message: "Invalid Credentials" });
+
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
     delete user.password;
     res.status(200).json({ token, user });
