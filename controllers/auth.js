1845fff182a15bbca1aee538772a4772ff5a2233
Anish <anishtharur@gmail.com>
2023-06-14 19:11:47 +0530

    deploy

--- controllers/auth.js
+++ controllers/auth.js
@@ -15,6 +15,7 @@ export const register = async (req, res) => {
       location,
       occupation,
     } = req.body;
+
     const salt = await bcrypt.genSalt();
     const passwordHash = await bcrypt.hash(password, salt);
 
